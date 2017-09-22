50784b31d74fa87285fefd765b2741bb3d1383fd
Rodrigo freitas <foolha@gmail.com>
2017-09-22 18:03:34 -0300

    backup-fedora

--- src/main/java/br/com/codepampa/rest/RestApplication.java
+++ src/main/java/br/com/codepampa/rest/RestApplication.java
@@ -1,9 +1,34 @@
 package br.com.codepampa.rest;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
+import br.com.codepampa.model.Categoria;
+import br.com.codepampa.service.CategoriaService;
 
 
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/api")
+public class RestApplication {
+
+
+   private CategoriaService categoriaService;
+
+    public RestApplication() {
+        categoriaService = new CategoriaService();
+    }
+
+
+    @GET
+    @Path("/categoria")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Categoria> listarClientes() {
+        return  categoriaService.findAllCriteria();
+    }
+
+
+
+
 }
